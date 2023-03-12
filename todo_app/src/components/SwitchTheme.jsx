13ee7dedbe8c74a33cf75d814e051b7033532983
darkspeaker <12nelson13@gmail.com>
2023-03-12 21:20:35 +0200

    fix: remove unnecessary wrapper

--- todo_app/src/components/SwitchTheme.jsx
+++ todo_app/src/components/SwitchTheme.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 export default function SwitchTheme(props) {
   return (
-    <div>
       <div
         className={`switch-theme switch-theme-${props.theme}`}
         onClick={props.toggleTheme}
       ></div>
-    </div>
   );
 }
